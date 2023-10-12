d94dde5be1b57f4f57d58d33212df895eb894031
unknown <vk9944836332@gmail.com>
2023-10-12 10:18:34 +0530

    calculator

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+
+
+
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input).toString());
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  return (
+    <body>
+    <div className="calculator">
+      <div className="display">
+        <input type="text" value={input} readOnly />
+        <span>{result}</span>
+      </div>
+      <div className="keypad">
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('0')}>0</button>
+        {/* <button onClick={() => handleClick('.')}>.</button> */}
+        <button onClick={() => handleClick('=')}>=</button>
+        <button onClick={() => handleClick('C')}>C</button>
+        <button onClick={() => handleClick('/')}>/</button>
+      </div>
+    </div>
+    </body>
+  );
+}
+
+export default App;
